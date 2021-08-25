1b7bd4f1ad3e092aece6022fd0f46c42a3cea1be
KalinkinFiz <kalinkin.fiz@gmail.com>
2021-08-26 01:51:07 +0300

    init: product-service

---
+++ product-service/serverless.ts
@@ -0,0 +1,33 @@
+import type { AWS } from "@serverless/typescript";
+
+import hello from "@functions/hello";
+
+const serverlessConfiguration: AWS = {
+  service: "product-service",
+  frameworkVersion: "2",
+  custom: {
+    webpack: {
+      webpackConfig: "./webpack.config.js",
+      includeModules: true,
+    },
+  },
+  plugins: ["serverless-webpack"],
+  provider: {
+    name: "aws",
+    runtime: "nodejs14.x",
+    region: "eu-west-1",
+    stage: "dev",
+    apiGateway: {
+      minimumCompressionSize: 1024,
+      shouldStartNameWithService: true,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
+    },
+    lambdaHashingVersion: "20201221",
+  },
+  // import the function via paths
+  functions: { hello },
+};
+
+module.exports = serverlessConfiguration;
