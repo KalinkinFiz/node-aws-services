362496569e4228199fea548d4d90d3c4fe509623
KalinkinFiz <kalinkin.fiz@gmail.com>
2021-10-10 16:44:23 +0300

    feat: add sls config

--- authorization-service/serverless.ts
+++ authorization-service/serverless.ts
@@ -1,31 +1,45 @@
-import type { AWS } from '@serverless/typescript';
+import type { AWS } from "@serverless/typescript";
 
-import hello from '@functions/hello';
+import basicAuthorizer from "@functions/basicAuthorizer";
 
 const serverlessConfiguration: AWS = {
-  service: 'authorization-service',
-  frameworkVersion: '2',
+  service: "authorization-service",
+  frameworkVersion: "2",
   custom: {
     webpack: {
-      webpackConfig: './webpack.config.js',
+      webpackConfig: "./webpack.config.js",
       includeModules: true,
     },
   },
-  plugins: ['serverless-webpack'],
+  plugins: ["serverless-webpack", "serverless-offline"],
   provider: {
-    name: 'aws',
-    runtime: 'nodejs14.x',
+    name: "aws",
+    runtime: "nodejs14.x",
+    region: "eu-west-1",
+    stage: "dev",
     apiGateway: {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
     },
     environment: {
-      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
     },
-    lambdaHashingVersion: '20201221',
+    lambdaHashingVersion: "20201221",
   },
   // import the function via paths
-  functions: { hello },
+  functions: { basicAuthorizer },
+  resources: {
+    Resources: {},
+    Outputs: {
+      BasicAuthorizerLambdaFunctionQualifiedArn: {
+        Export: {
+          Name: {
+            "Fn::Sub": "${AWS::StackName}-BasicAuthorizerArn",
+          },
+        },
+      } as any,
+    },
+  },
 };
 
 module.exports = serverlessConfiguration;
